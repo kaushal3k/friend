2acae9e995ca42c9b7e193c20cffa88be03abbb0
kaushal3k <kumarkaushal313@gmail.com>
2024-03-01 18:20:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var istatus = document.querySelector("h5")
+
+var addf = document.querySelector("#add")
+
+var check = 0
+
+
+addf.addEventListener("mouseover" , function(){
+    if (check == 0){
+        istatus.innerHTML= "Friends"
+        istatus.style.color = "green"
+        addf.innerHTML = "remove friends"
+        console.log("hello")
+        check = 1
+  } else{
+    istatus.innerHTML= "stranger"
+    istatus.style.color = "red"
+    addf.innerHTML = "Add events"
+    console.log("hello0")
+    check = 0
+  }
+   
+})
+
+
+
